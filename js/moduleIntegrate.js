7b7811d9c45be8581e0967999a9c765c79b3b736
AKO <arisv.ko@gmail.com>
2022-07-07 14:29:41 +0800

    Added moduleIntegrate.js

---
+++ js/moduleIntegrate.js
@@ -0,0 +1,8 @@
+function readHTML(){
+    const moduleContainer = document.querySelectorAll('[data-module]');
+    moduleContainer.forEach(modules => {
+        const refHTML = modules.dataset.module;
+        fetch(`${refHTML}`).then(response => response.text()).then(text => modules.innerHTML = text);        
+    });    
+}
+readHTML();

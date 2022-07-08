90d9fde8752e3258026400efa078a5ce6d7f9ec4
AKO <arisv.ko@gmail.com>
2022-07-08 15:38:11 +0800

    Created Home page js

---
+++ src/js/home.js
@@ -0,0 +1,31 @@
+const articleListContainer = '../json/articleList.json';
+
+async function fetchArticles(){
+    const request = new Request(articleListContainer);
+    const response = await fetch(request);
+    const articles = await response.json();            
+    createArticles(articles)
+} 
+function createArticles(articles){
+    let articleContainer = document.querySelector(".article-list");    
+    articles.articles.forEach( article => {
+        articleContainer.innerHTML+=`
+        <section class="section-item ${article.type}">
+            <img src="../../img/${article.img}">
+            <div>
+                <h1>${article.header}</h1>
+                <p>${article.content}</p>
+            </div>
+            <p class="date-item">${article.date}</p>
+        </section>
+        `
+    });
+}
+function handleCategoryClick(e){        
+    const articleContainer = e.closest('.article-list');
+    const activeButton = document.querySelector('.active');
+    console.log(activeButton);
+    activeButton && activeButton.classList.remove('active');
+    e.classList.add('active');
+    articleContainer.dataset.category = e.textContent.toLowerCase();
+}

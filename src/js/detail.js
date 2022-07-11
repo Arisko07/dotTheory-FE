4aed4c7d2bffa2041a68165049bbf7536c6e86d9
AKO <arisv.ko@gmail.com>
2022-07-11 11:04:31 +0800

    Move readHTML function

--- src/js/detail.js
+++ src/js/detail.js
@@ -69,12 +69,3 @@ function openNav() {
         arrow.classList.remove("left");
     }
 }
-
-function readHTML(){
-    const moduleContainer = document.querySelectorAll('[data-module]');
-    moduleContainer.forEach(modules => {
-        const refHTML = modules.dataset.module;
-        fetch(`${refHTML}`).then(response => response.text()).then(text => modules.innerHTML = text);        
-    });    
-}
-readHTML();
